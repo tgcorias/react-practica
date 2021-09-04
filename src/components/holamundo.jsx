499eaab4a7ff1dc33c700e8c41f739a9a98d5626
tgcorias <ticianocorias@gmail.com>
2021-09-04 18:19:01 -0300

    Cambios iniciales tardíos

---
+++ src/components/holamundo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles/app.scss';
+
+const HolaMundo = () => (
+    <h1>holiwis</h1>
+    );
+
+export default HolaMundo;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HolaMundo from './components/holamundo';
+
+ReactDOM.render(
+    <HolaMundo/>,
+    document.getElementById("app")
+)
